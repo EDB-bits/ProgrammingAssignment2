cdd8f9e9bf72d46bf390dea77230830d9fc91901
EDB-bits <elda.dereselegn1@rail.bombardier.com>
2021-07-05 17:38:37 +0300

    Assignment submisstion

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setreverse <- function(inverse) m <<- inverse
+        getreverse <- function() m
+        list(set = set, get = get,
+             setreverse = setreverse,
+             getreverse = getreverse)
+        
+        
 }
 
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getreverse()
+        if(!is.null(m)){
+                message("gettimg cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setreverse(m)
+        m
 }
